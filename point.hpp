6a0e5b21ffc346bb1dbe122912eef8f081d06ac8
Pessimist-hub <azbuca.vadim@gmail.com>
2026-09-20 00:24:42 +0300

    Modificare point.hpp

--- point.hpp
+++ point.hpp
@@ -1,7 +1,15 @@
 #pragma once
+#include <cmath>
 
-struct Point{
-   int x;
-   int y;
+struct Point {
+    float x = 0;
+    float y = 0;
+    float z = 0;
+
+    float DistanceTo(Point other) {
+        float dx = x - other.x;
+        float dy = y - other.y;
+        float dz = z - other.z;
+        return std::sqrt(dx * dx + dy * dy + dz * dz);
+    }
 };
-
